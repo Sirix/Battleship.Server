2bca5f43d0ece8ae00d2945c31a15e2b11c4fd63
Sirix <Sirix25@gmail.com>
2012-10-16 21:17:04 +0300

    Initial commit

---
+++ IServerContract.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace Battleship.Server
+{
+    [ServiceContract(SessionMode = SessionMode.Required, CallbackContract = typeof (IPlayerCallback))]
+    public interface IServerContract
+    {
+        [OperationContract]
+        Guid RegisterClient(string playerName);
+
+        [OperationContract]
+        void StartGame(Guid playerId, bool[][] field);
+
+        [OperationContract(IsOneWay = true)]
+        void LeaveGame(Guid playerId);
+
+        [OperationContract]
+        ShootResult Shoot(Guid playerId, Cell cell);
+    }
+
+    public interface IPlayerCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void SendStatus(PlayerStatus status);
+    }
+
+    public enum PlayerStatus
+    {
+        None = 0,
+        YourTurn = 1,
+        EnemyTurn = 2,
+        YouWin = 3,
+        EnemyWin = 4
+    }
+
+    public enum ShootResult
+    {
+        NonSpecified = 0,
+        Miss = 1,
+        Damaged = 2,
+        Destroyed = 3
+    }
+}
