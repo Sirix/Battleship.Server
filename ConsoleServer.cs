9da4edcf7d79c41f4a821d2e96d107cd411347dd
Sirix <Sirix25@gmail.com>
2012-10-18 12:33:28 +0300

    Fixed app config. Added some implementation to ServerService

--- ConsoleServer.cs
+++ ConsoleServer.cs
@@ -7,36 +7,25 @@ namespace Battleship.Server
 {
     public class ConsoleServer
     {
-        static void Main()
+        public static void Main()
         {
-            var baseAddress = new Uri("http://localhost:8080/Battleship");
-
-            // Create the ServiceHost.
-            using (var host = new ServiceHost(typeof(ServerContract), baseAddress))
+            try
             {
-                // Enable metadata publishing.
-                var smb = new ServiceMetadataBehavior
-                              {
-                                  HttpGetEnabled = true,
-                                  MetadataExporter =
-                                      {
-                                          PolicyVersion = PolicyVersion.Policy15
-                                      }
-                              };
-                host.Description.Behaviors.Add(smb);
+                // Create the ServiceHost.
+                using (var host = new ServiceHost(typeof(ServerService)))
+                {
+                    host.Open();
+                    Console.WriteLine("Press <Enter> to stop the service.");
+                    Console.ReadLine();
 
-                // Open the ServiceHost to start listening for messages. Since
-                // no endpoints are explicitly configured, the runtime will create
-                // one endpoint per base address for each service contract implemented
-                // by the service.
-                host.Open();
-
-                Console.WriteLine("The service is ready at {0}", baseAddress);
-                Console.WriteLine("Press <Enter> to stop the service.");
-                Console.ReadLine();
-
-                // Close the ServiceHost.
-                host.Close();
+                    // Close the ServiceHost.
+                    host.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.Read();
             }
         }
     }
---
+++ Services/ServerService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using Battleship.Server.Enums;
+using Battleship.Server.Interfaces;
+
+namespace Battleship.Server.Services
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class ServerService : IServerContract
+    {
+        private Dictionary<string, Guid> _connectedPlayers;
+
+        public ServerService()
+        {
+            _connectedPlayers = new Dictionary<string, Guid>();
+        }
+
+        public Guid RegisterClient(string playerName)
+        {
+            if (_connectedPlayers.ContainsKey(playerName))
+                throw new ArgumentException("Player with this name is already connected");
+
+            var guid = Guid.NewGuid();
+            _connectedPlayers.Add(playerName, guid);
+
+            return guid;
+        }
+
+        public void StartGame(Guid playerId, bool[][] field)
+        {
+            //TODO: Just test code
+            var _callback = OperationContext.Current.GetCallbackChannel<IPlayerCallback>();
+            _callback.SendStatus(PlayerStatus.YourTurn);
+        }
+
+        public void LeaveGame(Guid playerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Enums.ShootResult Shoot(Guid playerId, int x, int y)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
