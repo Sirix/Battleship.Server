5a049dc9f052f84aedf04715b7117aee4c54aca7
Ivan Manzhos <ivan.manzhos@gmail.com>
2012-10-20 23:48:38 +0400

    Rewrited RegisterClient

---
+++ Services/Player.cs
@@ -0,0 +1,16 @@
+﻿using Battleship.Server.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Battleship.Server
+{
+    class Player
+    {
+        public Guid PlayerId { get; set; }
+        public string Name { get; set; }
+
+        public IPlayerCallback CallBack { get; set; }
+    }
+}
--- Services/ServerService.cs
+++ Services/ServerService.cs
@@ -11,29 +11,34 @@ namespace Battleship.Server.Services
     [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
     public class ServerService : IServerContract
     {
-        private Dictionary<string, Guid> _connectedPlayers;
+        private List<Player> _players;
 
         public ServerService()
         {
-            _connectedPlayers = new Dictionary<string, Guid>();
+            _players = new List<Player>();
         }
 
         public Guid RegisterClient(string playerName)
         {
-            if (_connectedPlayers.ContainsKey(playerName))
+            if (_players.Any(p => p.Name == playerName))
                 throw new ArgumentException("Player with this name is already connected");
 
+            var callback = OperationContext.Current.GetCallbackChannel<IPlayerCallback>();
+
             var guid = Guid.NewGuid();
-            _connectedPlayers.Add(playerName, guid);
+            _players.Add(new Player { Name = playerName, PlayerId = guid, CallBack = callback });
 
             return guid;
         }
 
         public void StartGame(Guid playerId, bool[][] field)
         {
-            //TODO: Just test code
-            var _callback = OperationContext.Current.GetCallbackChannel<IPlayerCallback>();
-            _callback.SendStatus(PlayerStatus.YourTurn);
+            if (!_players.Any(p => p.PlayerId == playerId))
+                throw new ArgumentException("Player with this name is not connected");
+
+            if(!ValidateField(field))
+                throw new ArgumentException("Invalid field");
+
         }
 
         public void LeaveGame(Guid playerId)
@@ -44,6 +49,11 @@ namespace Battleship.Server.Services
         public Enums.ShootResult Shoot(Guid playerId, int x, int y)
         {
             throw new NotImplementedException();
+        }
+
+        private bool ValidateField(bool[][] field)
+        {
+            return true;
         }
     }
 }
